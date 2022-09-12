9452a30838a7d9037534d376a614160105686739
Sinisteer <112796568+Sinisteer@users.noreply.github.com>
2022-09-12 17:03:08 +0700

    Update database.js

--- database.js
+++ database.js
@@ -1,14 +1,34 @@
-async function connectData() {
-  const { MongoClient, ServerApiVersion } = require('mongodb');
-  const uri = process.env.DatabaseURL;
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-  client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-    
-    console.log("[Database]: Connected to Database.")
-  });
+async function connectData(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://Sinisteer_Dev:Sinisteer001@preciousbot.lwcbch3.mongodb.net/test?retryWrites=true&w=majority";
+ 
+
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
 }
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+main().catch(console.error);
 
 module.exports = {connectData}
